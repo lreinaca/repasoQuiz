322323f7a39d2aa660ce46d02661634bb59075a4
Rossibel <ibarrarossibel@gmail.com>
2025-02-27 21:26:57 -0500

    Update llamar metodo sumar

--- src/calculadorarepaso/Ejecutable.java
+++ src/calculadorarepaso/Ejecutable.java
@@ -8,6 +8,10 @@ package calculadorarepaso;
 public class Ejecutable {
     public static void main(String[] args) {
         System.out.println("HOLA ESTA ES UNA PRUEBA ");
+        double a = 2;
+        double b = 5;
+        
+        System.out.println("sumar: " + Main.sumar(a, b));
     }
 
 }
