aa978201ee245cb9f0eb3621923e4363760a5e39
LorenaReina <lorenareina2016@gmail.com>
2025-02-27 21:10:56 -0500

    Add changes in feature-ejecutable

---
+++ src/calculadorarepaso/Ejecutable.java
@@ -0,0 +1,13 @@
+
+package calculadorarepaso;
+
+/**
+ *
+ * 
+ */
+public class Ejecutable {
+    public static void main(String[] args) {
+        System.out.println("HOLA ESTA ES UNA PRUEBA ");
+    }
+
+}
